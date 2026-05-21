30be6339ca0694ee79f689616ed3438cb83b2bde
gen <genice6776@hotmail.com>
2026-05-21 02:46:36 -0400

    form updates

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ window.addEventListener("load", function() {
     const form = document.getElementById('my-form');
     const success_msg = document.getElementById('success-msg');
     form.addEventListener("submit", function(e) {
+        success_msg.style.display = "none";
         e.preventDefault();
         const data = new FormData(form);
         const action = e.target.action;
@@ -12,7 +13,7 @@ window.addEventListener("load", function() {
         })
         .then(() => {
         success_msg.style.display = "block";
-        form.style.display = "none";
+        // form.style.display = "none";
         })
     });
 });
